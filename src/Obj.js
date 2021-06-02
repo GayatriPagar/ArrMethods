5943c31bc5a28f467b3b01c3758cc3b62dcda10c
GayatriPagar <pagargayatri195@gmail.com>
2021-06-03 01:33:55 +0530

    first commit

---
+++ src/Obj.js
@@ -0,0 +1,14 @@
+// function Obj(props)
+// {
+// return(
+//  <div>
+//    <span>{props.data.name}</span>
+//    <span>{props.data.email}</span>
+//    <span>{props.data.Contact}</span>
+//  </div>
+// );
+
+// }
+// export default Obj;
+
+
